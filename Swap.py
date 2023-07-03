b58eea8a7b2fcdf58b3ef03843a388b6f843469f
Shaurya-Srivastav <ssrivastav@ucdavis.edu>
2023-07-02 23:14:12 -0700

    tuple swap

--- Swap.py
+++ Swap.py
@@ -31,3 +31,24 @@ def swap_function_tempVar(list, idx1, idx2):
     
 list2 = [1,3,5,8,10,2]
 print(swap_function_tempVar(list2, pos1, pos2)) 
+
+'''
+Simple swap to swap elements at a given position, using Tuples.
+Store the element at idx1 and idx2 as a pair in a tuple. 
+Unpack those elements with idx2 and idx1 positions in that list. 
+Both the positions in that list are swapped. 
+
+Big O Time Complexity: O(1), for using constant operations.
+
+'''
+def swap_function_tuples(list, idx1, idx2):
+    tupleswap = list[idx1], list[idx2]
+    
+    list[idx2], list[idx1] = tupleswap
+    
+    return list
+
+   
+list3 = [1,3,5,8,10,2]
+print(swap_function_tuples(list3, pos1, pos2)) 
+    
